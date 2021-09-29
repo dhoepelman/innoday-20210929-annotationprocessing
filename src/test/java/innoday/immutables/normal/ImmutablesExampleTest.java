ae4c1111f45ac035b5c7acc44dba5fd18cf5e7d5
David Hoepelman <dhoepelman@xebia.com>
2021-09-29 10:44:07 +0200

    Add wither

--- src/test/java/innoday/immutables/normal/ImmutablesExampleTest.java
+++ src/test/java/innoday/immutables/normal/ImmutablesExampleTest.java
@@ -18,8 +18,18 @@ public class ImmutablesExampleTest {
   @Test
   public void testBuilder() {
     var example =
-        ImmutableExample.builder().id("1").addNumbers(4, 5, 6).comment("Just some example");
+        ImmutableExample.builder().id("1").addNumbers(4, 5, 6).comment("Just some example").build();
 
     assertThat(example).isNotNull();
+  }
+
+  @Test
+  public void testWither() {
+    Example original = ImmutableExample.builder().id("1").build();
+
+    Example modified = original.withNumbers(1, 2, 3);
+
+    assertThat(original.numbers()).isEqualTo(List.of());
+    assertThat(modified.numbers()).isEqualTo(List.of(1, 2, 3));
   }
 }
